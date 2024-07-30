8699945c39af92a688649a246a33e30ab8ed2780
Олег Селін <givemetheweekend@gmail.com>
2024-07-30 22:50:21 +0300

    com

--- 03-theory-num-str-date/02-strings.js
+++ 03-theory-num-str-date/02-strings.js
@@ -1,4 +1,4 @@
-const name = "Ihor";
+const firstName = "Ihor";
 const age = 2;
 
 function getAge() {
@@ -6,8 +6,24 @@ function getAge() {
 }
 
 // const old = "hello my name is " + name + " and my age is " + age;
-const output = `hello my is ${name} and my age is ${
-  getAge() > 18 ? "Can drive" : `Can't drive`
-}`;
+// const output = `hello my is ${name} and my age is ${
+//   getAge() > 18 ? "Can drive" : `Can't drive`
+// }`;
 
-console.log(output);
+// console.log(output);
+
+console.log(firstName.length)
+console.log(firstName.toUpperCase())
+console.log(firstName.toLowerCase())
+console.log(firstName.charAt(0))
+console.log(firstName.indexOf('h'))
+console.log(firstName.indexOf('1'))
+console.log(firstName.toLowerCase().startsWith('ih'))
+console.log(firstName.endsWith('r'))
+console.log(firstName.repeat(2))
+
+const password = 'Oleh Pro'
+
+console.log(password.trim())
+console.log(password.padStart(10))
+
