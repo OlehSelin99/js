15d80db22f96e95507306436e516b36c56af1861
Олег Селін <givemetheweekend@gmail.com>
2024-08-21 14:31:56 +0300

    con

--- 06-result-start/app.js
+++ 06-result-start/app.js
@@ -12,17 +12,24 @@
 const list = document.querySelector("#list");
 
 async function start() {
+  list.innerHTML = "Loading ...";
   try {
     const resp = await fetch("https://jsonplaceholder.typicode.com/users", {
       method: "GET",
     });
     const data = await resp.json();
     console.log(data);
-  } catch (err) {}
+    setTimeout(() => {
+      render(data);
+    }, 2000);
+  } catch (err) {
+    list.innerHTML = err.message;
+  }
 }
 
 function render(users = []) {
-  
+  const html = users.map(toHTML).join(""); // html is array
+  list.innerHTML = html;
 }
 
 function toHTML(user) {
